fc3a39422333d2274d66eea7cfb3054239e70321
AHN HO JUNG <labeldock@me.com>
2017-12-28 17:07:18 +0900

    initial commit

---
+++ public/js/vue-trainman.js
@@ -0,0 +1,132 @@
+(function(global, factory){
+    
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.VueTrainman = factory());
+    
+}(this, function(){
+    var VueTrainman = function(){};
+    
+    VueTrainman.install = function(Vue, options){
+        
+        var VueMixin = {};
+        
+        Vue.prototype.$train = {
+            toggle:function(ta,cv,set){
+                var index = -1;
+                for(var i=0,l=ta.length;i<l;i++) if(ta[i] === cv) {index=i+1;break;}
+                if(arguments.length > 2) for(var i=0,l=ta.length;i<l;i++) if( ta[i] == set ) return ta[i];
+                index = ta.length == index ? 0 : index;
+                return ta[index];
+            }
+        };
+        
+        var AutoPopCache = (function(){
+            var AutoPopCache = function(){
+                this.$cache = [];
+            };
+            
+            AutoPopCache.prototype = {
+                $append:function(meta){
+                    typeof meta === "object" && this.$cache.push(meta);
+                }
+            };
+            
+            return (new AutoPopCache());
+        }());
+        
+        VueMixin.beforeRouteUpdate = function(to,from,next){
+            AutoPopCache.$cache = AutoPopCache.$cache.filter(function(meta){
+                if(meta.fullPath === to.fullPath){
+                    meta.pop();
+                    return false;
+                }
+                return true;
+            });
+            next();
+        };
+        
+        // $track
+        VueMixin.created = function(){
+            this.$track = {};
+        };
+        
+        function cloneDeep(obj){
+            try {
+                return JSON.parse(JSON.stringify(obj));
+            } catch(e) {
+                console.error("Problemalling",obj);
+                throw e;
+            }
+        };
+        
+        function touchTrack(vueInstance,key){
+            if(!vueInstance.$track){
+                vueInstance.$track = {};
+            }
+            if(!vueInstance.$track[key]){
+                vueInstance.$track[key] = [];
+            }
+            return vueInstance.$track[key];
+        };
+        
+        Vue.prototype.$stash = function(key,autoPop){
+            if(!key) return console.warn("require string type track key",key);
+            var trackDock = touchTrack(this,key);
+            
+            if(trackDock){
+                trackDock.push(cloneDeep(this[key]));
+            }
+            
+            if(this.$route && autoPop === true){
+                AutoPopCache.$append({
+                    fullPath:this.$route.fullPath,
+                    pop:function(){ this.$pop(key); }.bind(this)
+                });
+            }
+        };
+        
+        Vue.prototype.$freez = function(key){
+            if(!key) return console.warn("require string type track key",key);
+            var trackDock = touchTrack(this,key);
+            if(trackDock){
+                Array.prototype.splice.call(trackDock,0,trackDock.length);
+            }
+        };
+        
+        Vue.prototype.$pop = function(key,revert){
+            if(!key) return console.warn("require string type track key",key);
+            var trackDock = touchTrack(this,key);
+            
+            if(trackDock && trackDock.length){
+                if(revert === false){
+                    return trackDock.pop();
+                } else {
+                    this[key] = trackDock.pop();
+                    return this[key];
+                }
+            }
+        };
+        
+        //router-link 디렉티브
+        Vue.directive("router-link",{
+            bind:function(el){
+                el.addEventListener("click",function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    var linkTo = el.getAttribute("to");
+                                
+                    if(linkTo){
+                        Vue.nextTick();
+                        router.push(linkTo);
+                    }
+                });
+            }
+        });
+        
+        Vue.mixin(VueMixin);
+    };
+    
+    return VueTrainman;
+}));
---
+++ server/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
+var path    = require('path');
+var io      = require('socket.io')(http);
+
+var server = {
+  ip4v:void 0,
+  protocal:"http",
+  port:9111
+}
+
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  server.ip4v = add;  
+});
+
+http.listen(server.port,function(){
+    console.log('Express server listening on ' + server.port);
+});
+
+app.use("/",express.static(path.resolve(__dirname,'../public')));
+
+app.get("/mount",function(req,res){
+  var mountdata = {server:server};
+  res.status(200).send("window.hotline="+JSON.stringify(mountdata));
+});
+
+var connection = io.on("connection", function(socket){
+  socket.on("hot:create",function(data){
+    connection.emit("hot:created",data);
+  });
+});
